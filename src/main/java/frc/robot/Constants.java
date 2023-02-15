0bde224dcaa5ae12e384a979cad0377ab2d95acc
Drive Station <drive.station@millbrookrobotics.com>
2023-02-15 15:55:09 -0500

    Changed drive and turn speed

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -27,8 +27,8 @@ public final class Constants {
   public static final class DriveConstants {
     // Driving Parameters - Note that these are not the maximum capable speeds of
     // the robot, rather the allowed maximum speeds
-    public static final double kMaxSpeedMetersPerSecond = 4.8;
-    public static final double kMaxAngularSpeed = 2 * Math.PI; // radians per second
+    public static final double kMaxSpeedMetersPerSecond = 3; //was 4.8
+    public static final double kMaxAngularSpeed = 1.5 * Math.PI; // radians per second, was 2
 
     // Chassis configuration
     public static final double kTrackWidth = Units.inchesToMeters(23.0);
