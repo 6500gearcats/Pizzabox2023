aabb371cc6642a24c888218d5a648335b7292d6e
Julia Gale <jrgale@students.wcpss.net>
2023-02-19 16:35:45 -0500

    Fixed merge error

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -97,7 +97,7 @@ public class RobotContainer {
         new Pose2d(3, 0, new Rotation2d(0)),
         config);
 
-    var thetaController = new ProfiledPIDController(
+        var thetaController = new ProfiledPIDController(
         AutoConstants.kPThetaController, 0, 0, AutoConstants.kThetaControllerConstraints);
     thetaController.enableContinuousInput(-Math.PI, Math.PI);
 
