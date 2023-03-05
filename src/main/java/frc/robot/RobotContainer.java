9bd3c793a0b25e068845a6b7bd5a0645ac7674cc
Drive Station <drive.station@millbrookrobotics.com>
2023-03-05 13:33:05 -0500

    Fixed Path Planner

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -101,7 +101,7 @@ public class RobotContainer {
   public Command getAutonomousCommand() {
    // This will load the file "FullAuto.path" and generate it with a max velocity of 4 m/s and a max acceleration of 3 m/s^2
     // for every path in the group
-    ArrayList<PathPlannerTrajectory> pathGroup = (ArrayList<PathPlannerTrajectory>) PathPlanner.loadPathGroup("FullAuto", new PathConstraints(4, 3));
+    ArrayList<PathPlannerTrajectory> pathGroup = (ArrayList<PathPlannerTrajectory>) PathPlanner.loadPathGroup("fullAuto", new PathConstraints(4, 3));
 
     // This is just an example event map. It would be better to have a constant, global event map
 // in your code that will be used by all path following commands.
