f9c5e22f80cda910388abfe7fb30b667324a415f
Robert Gale <robagale@aol.com>
2023-03-10 22:37:38 -0500

    Implement Mid and High Arm button mapping

--- src/main/java/frc/robot/commands/MoveArmToPosition.java
+++ src/main/java/frc/robot/commands/MoveArmToPosition.java
@@ -1,10 +1,7 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.FunctionalCommand;
-import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import edu.wpi.first.wpilibj2.command.WaitCommand;
-import frc.robot.Constants.ArmConstants;
 import frc.robot.subsystems.Arm;
 
 /**
