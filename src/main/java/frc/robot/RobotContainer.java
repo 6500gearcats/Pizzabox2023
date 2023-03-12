0cae4733facf03b38d17158a39fe9d2243d83aa9
Nicholas Rivera <nicriver0501@gmail.com>
2023-03-12 09:53:15 -0400

    changed robo size in pathplanner

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -206,11 +206,11 @@ public class RobotContainer {
     {
       System.out.println("Starting Path 1");
       return new CloseClaw(m_Claw).withTimeout(0.5)
-      .andThen(new MoveArmToPosition(ArmConstants.kArmHighAngle, m_Arm))
+      .andThen(new MoveArmToPosition(ArmConstants.kArmHighAngle, m_Arm)).withTimeout(5.0)
       .andThen(cubePath1_1
       .andThen(new OpenClaw(m_Claw).withTimeout(0.5))      
       .andThen(cubePath1_2
-      .andThen(new MoveArmToPosition(ArmConstants.kArmStowAngle, m_Arm))
+      .andThen(new MoveArmToPosition(ArmConstants.kArmStowAngle, m_Arm)).withTimeout(5.0)
       .andThen(cubePath1_3
       .andThen(pathEnd1
       .andThen(new ClimbPlatform(m_robotDrive, m_Gyro)
@@ -221,11 +221,11 @@ public class RobotContainer {
     {
       System.out.println("Starting Path 2");
       return new CloseClaw(m_Claw).withTimeout(0.5)
-      .andThen(new MoveArmToPosition(ArmConstants.kArmHighAngle, m_Arm))
+      .andThen(new MoveArmToPosition(ArmConstants.kArmHighAngle, m_Arm)).withTimeout(5.0)
       .andThen(cubePath2_1
-      .andThen(new OpenClaw(m_Claw).withTimeout(0.5))
+      .andThen(new OpenClaw(m_Claw).withTimeout(0.5))      
       .andThen(cubePath2_2
-      .andThen(new MoveArmToPosition(ArmConstants.kArmStowAngle, m_Arm))
+      .andThen(new MoveArmToPosition(ArmConstants.kArmStowAngle, m_Arm)).withTimeout(5.0)
       .andThen(cubePath2_3
       .andThen(pathEnd1
       .andThen(new ClimbPlatform(m_robotDrive, m_Gyro)
@@ -236,15 +236,15 @@ public class RobotContainer {
     {
       System.out.println("Starting Path 3");
       return new CloseClaw(m_Claw).withTimeout(0.5)
-      .andThen(new MoveArmToPosition(ArmConstants.kArmHighAngle, m_Arm))
+      .andThen(new MoveArmToPosition(ArmConstants.kArmHighAngle, m_Arm)).withTimeout(5.0)
       .andThen(cubePath3_1
-      .andThen(new OpenClaw(m_Claw).withTimeout(0.5))
+      .andThen(new OpenClaw(m_Claw).withTimeout(0.5))      
       .andThen(cubePath3_2
-      .andThen(new MoveArmToPosition(ArmConstants.kArmStowAngle, m_Arm))
+      .andThen(new MoveArmToPosition(ArmConstants.kArmStowAngle, m_Arm)).withTimeout(5.0)
       .andThen(cubePath3_3
       .andThen(pathEnd1
       .andThen(new ClimbPlatform(m_robotDrive, m_Gyro)
-      .andThen(()-> m_robotDrive.drive(0, 0, 0, false 
+      .andThen(()-> m_robotDrive.drive(0, 0, 0, false
       )))))));
     }
     else
